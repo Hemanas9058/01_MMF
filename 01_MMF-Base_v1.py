3d48dd7feb7188cf39a26b6fdc395d852fe096f3
Hemanas9058 <78828656+Hemanas9058@users.noreply.github.com>
2022-02-10 14:42:51 +1300

    Add files via upload

---
+++ 01_MMF-Base_v1.py
@@ -0,0 +1,29 @@
+# import statements
+
+# functions
+
+
+# Main Routine
+
+# set up dictionaries / lists needed to hold data
+
+# ask user if they have the program before & show instructions if necessary
+
+# loop to get ticket details
+
+    # get name (can't be blank)
+
+    # get age (between 12 and 130)
+
+    # calculate ticket price
+
+    # loop to ask for snacks
+
+    # calculate snack price
+
+    # ask for payment method (and apply surcharge if necessary)
+
+    
+# Calculate Total sales and profit
+
+# Output data to text file
